cb53df38f202055aaab7bdcc0b6a1a59e2c68691
Rodrigo Pronunciati <rodpro@hotmail.com>
2022-09-15 09:51:31 -0300

    alterações css e conteúdo

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Android 13 terá função que monitora saúde da bateria dos aparelhos",
+    "description": "O Google lançou a versão beta da primeira atualização trimestral do Android 13, e embora tenha foco em melhorar a estabilidade do sistema operacional, há uma variedade de novos recursos que devem chegar aos aparelhos elegíveis. Conforme descoberto por Mishaal Rahman, uma das novidades é o monitor para a saúde da bateria dos dispositivos. O desenvolvedor publicou na última quinta-feira (08) a captura de tela de um recurso chamado de “Battery Health” (“Saúde da Bateria”, em tradução livre), que exibirá a integridade da bateria ao longo do tempo. Quando o componente estiver com baixa capacidade, o sistema irá recomendar sua substituição.",
+    "photoCover":"https://t2.tudocdn.net/615875?w=646&h=284"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "iOS 16 chegou! Conheça 8 novidades que vão mudar seu iPhone",
+    "description": "O iPhone vai ganhar uma cara nova a partir da versão iOS 16 de seu sistema operacional. A novidade poderá ser baixada a partir de hoje - no Brasil a nova versão será disponibilizada à tarde. Entre as principais mudanças estão a tela de bloqueio, que vai permitir mais personalização e o recurso de fotos, que vai possibilitar que pais e responsáveis tenham maior controle sobre as imagens armazenadas nos dispositivos dos filhos.",
+    "photoCover":"https://conteudo.imguol.com.br/c/noticias/74/2022/06/06/telas-do-novo-ios-16-1654541353914_v2_900x506.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Galaxy Z Fold 4 é mais prático, potente e gigante; mas serve para quem?",
+    "description": "\"Você compraria o Galaxy Z Fold 4?\". Essa foi uma das perguntas que mais ouvi ao utilizar o celular com tela dobrável da Samsung. A resposta simples para a questão é: não. Isso quer dizer que ele é ruim? De jeito nenhum. As câmeras do Z Fold 4 são ótimas, a tela é impressionantemente grande e com alto nível de brilho. Sem contar o desempenho do smartphone, que tem um processador surreal de bom. O fato é que não é um telefone para mim — não só pela questão financeira (ele custa quase R$ 13 mil) —, mas por ele ser gigante. E é justamente esta característica que pode atrair diferentes públicos para ele. Pessoalmente, prefiro telefones menores — como o Z Flip 4.",
+    "photoCover":"https://conteudo.imguol.com.br/c/noticias/01/2022/08/09/celular-dobravel-galaxy-z-fold-4-aberto-1660088227450_v2_900x506.jpg"
+  },
+  {
+    "id":"4",
+    "title": "DJI lança câmera Osmo Action 3 com vídeos em 4K a 120 fps e mais bateria",
+    "description": "A DJI atualizou a sua linha de câmeras de ação com o novo modelo Osmo Action 3. O produto ganhou algumas melhorias de construção e uma nova bateria mais duradoura, entre outras novidades. O tamanho da bateria foi um dos pontos mais criticados no modelo Action 2, seu antecessor direto. Sem os módulos adicionais, ela oferecia apenas 60 minutos ininterruptos de uso, tempo que agora foi estendido para até 160 minutos com a chamada “Extreme Battery”. Além disso, o tanque também conta com compatibilidade para recargas mais rápidas. De acordo com informações apontadas pela DJI, é possível ter uma carga de 80% após apenas 18 minutos, ou carga total em aproximadamente 50 minutos.",
+    "photoCover":"https://t.ctcdn.com.br/uCd85RWSV6uCFuMLQexJQlhuHAo=/720x405/smart/filters:format(webp)/i631363.png"
   }
 ]
